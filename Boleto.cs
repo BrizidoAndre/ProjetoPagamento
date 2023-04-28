4302a8664ac684f08eaf32ba934d41be3feae408
Andre <abrizidobasilio@gmail.com>
2023-04-28 10:25:22 -0300

    finalizando Boleto

---
+++ Boleto.cs
@@ -0,0 +1,15 @@
+using ProjetoPagamento;
+namespace ProjetoPagamento
+{
+    public class Boleto
+    {
+        
+        private string codigoBarras;
+        private float desconto = 0.88F;
+        public void Registrar(float valorPago, DateTime data){
+            float valorBoleto = (float) Math.Round(valorPago * desconto, 2);
+            Console.WriteLine($"No dia de {data}");
+            Console.WriteLine($"O total a ser pago é de: R$ {valorBoleto}");
+        }
+    }
+}
