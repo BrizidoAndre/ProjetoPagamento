edd7e4e0fd2e70d86d23cf61a93d870779a9f4e1
GCRusso <gabriel.coral.russo@gmail.com>
2023-05-01 22:35:18 -0300

    feito a parte menu de bandeiras

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// int opcao = '0';
+﻿string bandeira = "";
+string opcao = "";
 
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine(@$"
@@ -16,39 +17,114 @@ Console.WriteLine(@$"
 ");
 Console.WriteLine($"Seleciona a opção desejada: ");
 Console.ResetColor();
-int opcao = int.Parse(Console.ReadLine());
-    
+opcao = Console.ReadLine();
 
 do
 {
     switch (opcao)
     {
-        case '1':
+        case "1":
 
             break;
 
-        case '2':
+
+
+
+
+        case "2":
+
+        do
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@$"
+ -------------------------------------------
+|     Selecione a bandeira do cartão        |
+|                                           |
+|   1- Visa                                 |
+|   2- MasterCard                           |
+|   3- Elo                                  |
+|   4- American Express                     |
+|                                           |
+|   0- Sair/Fechar                          |
+ -------------------------------------------
+        ");
+
+            Console.WriteLine($"Selecione a bandeira:");
+
+            bandeira = Console.ReadLine();
+            Console.ResetColor();
+            switch (bandeira)
+            {
+                case "1":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Bandeira selecionada: Visa");
+                    Console.ResetColor();
+                    break;
+
+                case "2":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Bandeira selecionada: MasterCard");
+                    Console.ResetColor();
+                    break;
+
+                case "3":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Bandeira selecionada: Elo");
+                    Console.ResetColor();
+                    break;
+
+                case "4":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Bandeira selecionada: American Express");
+                    Console.ResetColor();
+                    break;
+
+                case "0":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Fechando... Obrigado!");
+                    Console.ResetColor();
+
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Opção inexistente! Tente novamente: ");
+                    Console.ResetColor();
+                    bandeira = Console.ReadLine();
+                    break;
+            }
+        } while (bandeira != "0");
+            break; 
+
+
+
+
+
+
+
+
+        case "3":
 
             break;
 
-        case '3':
+        case "4":
 
             break;
 
-        case '4':
-
-            break;
-
-        case '0':
+        case "0":
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Fechando... Obrigado!");
+            Console.ResetColor();
 
             break;
         default:
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"Opção inexistente! Tente novamente: ");
             Console.ResetColor();
-            opcao = int.Parse(Console.ReadLine());
+            opcao = Console.ReadLine();
             break;
     }
 
 
-} while (opcao != '0');
+} while (opcao != "0");
