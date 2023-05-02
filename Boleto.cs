27ce7d2fafb19400329864924e6cd812f88b552d
André <abrizidobasilio@gmail.com>
2023-05-02 00:12:52 -0300

    Finalizando o código de barras e linkando a classe Boleto com a classe Pagamento

--- Boleto.cs
+++ Boleto.cs
@@ -1,13 +1,26 @@
 using ProjetoPagamento;
 namespace ProjetoPagamento
 {
-    public class Boleto
+    public class Boleto : Pagamento
     {
-        private string codigoBarras;
+        private Random random = new Random();
+        public string CodigoDeBarras(){
+            int num1 = random.Next(1 , 2000000000);
+            int num2 = random.Next(1 , 2000000000);
+            int num3 = random.Next(1 , 2000000000);
+            string codigo = @"
+            -------------
+              " + num1.ToString() + @"
+              " + num2.ToString() + @"
+              " + num3.ToString() + @"
+            -------------";
+            return codigo;
+        }
         private float desconto = 0.88F;
-        public void Registrar(float valorPago, DateTime data){
-            float valorBoleto = (float) Math.Round(valorPago * desconto, 2);
-            Console.WriteLine($"No dia de {data}");
+        public void Registrar()
+        {
+            float valorBoleto = (float)Math.Round(valor * desconto, 2);
+            Console.WriteLine($"No dia de {date1}");
             Console.WriteLine($"O total a ser pago é de: R$ {valorBoleto}");
         }
     }
