1ce1a4fb4e575a579bd2c7c27873a03fb9e2dd82
juliaathar <juathar@gmail.com>
2023-05-02 07:48:19 -0300

    merge com develop

--- PagamentoDebito.cs
+++ PagamentoDebito.cs
@@ -3,10 +3,13 @@ namespace ProjetoPagamento
 {
     public class PagamentoDebito
     {
-
         private float saldo = 3000;
+        public string CVV { get; set; }
+        public string Titular { get; set; }
+        public string NumeroCartao { get; set; }
+        public string Bandeira { get; set; }
 
-        public void EfetuarPagamento(float valor, float saldoFinal)
+        public void Pagar(float valor, float saldoAtual)
         {
             if (valor > saldo)
             {
@@ -14,38 +17,30 @@ namespace ProjetoPagamento
             }
             else if (saldo >= valor)
             {
-                saldoFinal = saldo - valor;
-                Console.WriteLine($"Pagamento de R${valor} realizado com sucesso. Novo saldo: R${saldoFinal}");
+                saldoAtual = saldo - valor;
+                Console.WriteLine($"Pagamento de R${valor} realizado com sucesso. Novo saldo: R${saldoAtual}");
             }
             else
             {
                 Console.WriteLine($"Erro! Tente novamente");
             }
         }
+
+        public void SalvarCartao()
+        {
+            Console.WriteLine("Digite o CVV do cartão:");
+            CVV = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do titular do cartão:");
+            Titular = Console.ReadLine();
+
+            Console.WriteLine("Digite o número do cartão:");
+            NumeroCartao = Console.ReadLine();
+
+            Console.WriteLine("Digite a bandeira do cartão:");
+            Bandeira = Console.ReadLine();
+
+            Console.WriteLine("Informações do cartão salvas com sucesso!");
+        }
     }
 }
-
-
-
-
-// Console.WriteLine($"Para realizarmos o pagamento do seu produto e preciso que nos informe os dados do cartao :");
-//             Console.WriteLine(@$"
-//              ___________
-//             |Informe a Bandeira do Cartao:|
-//             |-----------------------------|
-//             |1) Visa                      |
-//             |2) MasterCard                |
-//             |3) Elo                       |
-//             |4) American Express          |
-//             |___________|
-//             ");
-//             Bandeira = Console.ReadLine();
-            
-//             Console.Write($"Informe o numero do cartao :");
-//             NumeroCartao = Console.ReadLine();
-
-//             Console.Write($"Informe o nome do Titular do cartao :");
-//             Titular = Console.ReadLine();
-            
-//             Console.WriteLine($"Informe o CVV do cartao a ser cadastrado (XXX) :");
-//             CVV = Console.ReadLine();
